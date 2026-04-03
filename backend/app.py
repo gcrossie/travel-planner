00dfb743eae82f3799511bf3ea411211e81cb757
Gabriella Cross <gabriella.cross@hotmail.com>
2026-04-03 18:49:30 +0200

    first commit

---
+++ backend/app.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI backend!"}
+
+trips = [
+    {"id": 1, "destination": "Paris", "status": "Packing"},
+    {"id": 2, "destination": "Tokyo", "status": "Planned"}
+]
+
+@app.get("/trips")
+def get_trips():
+    return trips
+
+from pydantic import BaseModel
+
+# Schema (struktur för data)
+class Trip(BaseModel):
+    destination: str
+    status: str
+
+# POST endpoint
+@app.post("/trips")
+def create_trip(trip: Trip):
+    new_trip = {
+        "id": len(trips) + 1,
+        "destination": trip.destination,
+        "status": trip.status
+    }
+    trips.append(new_trip)
+    return new_trip
+
+@app.get("/trips/{trip_id}")
+def get_trip(trip_id: int):
+    for trip in trips:
+        if trip["id"] == trip_id:
+            return trip
+    return {"error": "Trip not found"}
